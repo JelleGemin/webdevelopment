8b6f5b78f5339e3bbbd149494c6f019c7598fa37
JelleGemin <jelle.gemin@student.vives.be>
2023-02-21 09:22:31 +0100

    js fix

--- labo 2 sem 2/opdracht_kopieer/opdracht_6.js
+++ labo 2 sem 2/opdracht_kopieer/opdracht_6.js
@@ -3,8 +3,8 @@ const setup = () => {
     btnKopieer.addEventListener("click", kopieer);
 }
 const kopieer = () => {
-    let txtInput = document.getElementById("txtInput");
-    let tekst = txtInput.value;
+    let txtInput = document.getElementById("txtInput").value;
+    let tekst = txtInput.toString();
     let pElement = document.getElementById("txtOutput");
     pElement.innerHTML = tekst;
 }
