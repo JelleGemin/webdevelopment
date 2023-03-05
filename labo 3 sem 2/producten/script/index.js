9e47b893b5fc5677713e7d595cfff3425dd523ac
JelleGemin <jelle.gemin@student.vives.be>
2023-03-05 10:29:49 +0100

    producten af

--- labo 3 sem 2/producten/script/index.js
+++ labo 3 sem 2/producten/script/index.js
@@ -1,3 +1,31 @@
 const setup = () => {
+    let btnBereken = document.getElementById("bereken");
+    btnBereken.addEventListener("click", bereken);
+}
+
+const bereken = () =>{
+
+    let aantallen = document.getElementsByClassName("aantal")
+    let aantalP1 = aantallen[0].value;
+    let aantalP2 = aantallen[1].value;
+    let aantalP3 = aantallen[2].value;
+
+    let prijzen = document.getElementsByClassName("prijs");
+    let prijs1 = parseFloat(prijzen[0].textContent.split(" ")[0]);
+    let prijs2 = parseFloat(prijzen[1].textContent.split(" ")[0]);
+    let prijs3 = parseFloat(prijzen[2].textContent.split(" ")[0]);
+
+    let subtotalen = document.getElementsByClassName("subtotaal");
+    let subtotaal1 = aantalP1 * prijs1;
+    let subtotaal2 = aantalP2 * prijs2;
+    let subtotaal3 = aantalP3 * prijs3;
+
+    subtotalen[0].innerHTML = subtotaal1;
+    subtotalen[1].innerHTML = subtotaal2;
+    subtotalen[2].innerHTML = subtotaal3.toFixed(2);
+
+    let totaal = document.getElementById("berekendTotaal")
+    totaal.innerHTML = (subtotaal1 + subtotaal2 + subtotaal3).toFixed(2);
+
 }
 window.addEventListener("load", setup);
