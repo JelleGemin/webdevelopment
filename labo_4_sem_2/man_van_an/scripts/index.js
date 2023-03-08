008ff28a90adf57118c71827348dda63a98c4be2
JelleGemin <jelle.gemin@student.vives.be>
2023-03-08 11:45:37 +0100

    de man van an AF

---
+++ labo_4_sem_2/man_van_an/scripts/index.js
@@ -0,0 +1,18 @@
+const setup = () => {
+    getAantalKeerAn();
+}
+
+const getAantalKeerAn = () => {
+    let inputTekst = document.getElementById("tekst").textContent;
+    let input = inputTekst.toLowerCase();
+    let aantal = 0;
+    if (input.indexOf("an") != -1) {
+        for (let i = 0; i < input.length; i++) {
+            if (input.charAt(i) === 'a' && input.charAt(i+1) === 'n') {
+                aantal++;
+            }
+        }
+    }
+    console.log(aantal);
+}
+window.addEventListener("load", setup);
